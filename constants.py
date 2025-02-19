57b1c8f687a84c9de278ece246be84a8bccfd2f9
Gašper Azinovič <gasper.azinovic@gmail.com>
2025-02-19 12:01:03 +0100

    updated requirements

--- constants.py
+++ constants.py
@@ -3,8 +3,8 @@ EXPANSION_FACTOR = 0.8
 GUIDANCE_SCALE = 7.5
 NUM_INFERENCE_STEPS = 21
 STRENGTH = 0.99
-INPUT_VIDEO = "/teamspace/studios/this_studio/4_aruco_moving_stairs.mp4"
-OUTPUT_VIDEO = "out_v1.1_aruco_moving_stairs.mp4"
+INPUT_VIDEO = "video/demo_input.mp4"
+OUTPUT_VIDEO = "video/demo_output.mp4"
 CROP_PERCENTAGE = 0.5
 
 # Fixed parameters for BIRCH clustering
